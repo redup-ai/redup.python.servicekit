7c438f3d900443b71bd81da28c6b5b200d5de518
Andrii Hraboviy <35834412+andriygav@users.noreply.github.com>
2026-02-23 14:23:59 +0300

    Update test_monitor_server.py

--- tests/test_monitor_server.py
+++ tests/test_monitor_server.py
@@ -1,3 +1,5 @@
+import requests
+
 import pytest
 
 from redup_servicekit.monitoring import MonitorServer
@@ -6,6 +8,20 @@ def test_running():
     MonitorServer().run()
     monitoring = MonitorServer.get_instance()
     assert monitoring is not None
+
+def test_prometheus():
+    prometheus_lines = requests.get(
+        'http://localhost:9999/metrics'
+    ).text.split('\n')
+
+    keys = set()
+    for prometheus_line in prometheus_lines:
+        prometheus_line = prometheus_line.strip()
+        if not prometheus_line.startswith('#') and len(prometheus_line) > 0:
+            keys.add(prometheus_line.split()[0].split('{')[0])
+
+    for key in ['started_total', 'completed_total']:
+        assert key in keys
 
 @pytest.mark.asyncio
 async def test_stats():
