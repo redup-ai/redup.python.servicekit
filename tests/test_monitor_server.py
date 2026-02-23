7d4b0aefa7b66ef0e473ceabf1c6f0f71edd7a73
Andrii Hraboviy <35834412+andriygav@users.noreply.github.com>
2026-02-23 14:17:58 +0300

    Update test_monitor_server.py

--- tests/test_monitor_server.py
+++ tests/test_monitor_server.py
@@ -1,6 +1,13 @@
+import pytest
+
 from redup_servicekit.monitoring import MonitorServer
 
-def test_monitor_server_running():
+def test_running():
     MonitorServer().run()
     monitoring = MonitorServer.get_instance()
     assert monitoring is not None
+
+@pytest.mark.asyncio
+async def test_stats():
+    monitoring = MonitorServer.get_instance()
+    assert len(await monitoring.get_stats()) == 0
