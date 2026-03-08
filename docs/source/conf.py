75a419ad84282049d5b538c215d61b1b95eef0d9
Andrii Hraboviy <35834412+andriygav@users.noreply.github.com>
2026-03-08 13:51:00 -0400

    try add favicon

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -72,6 +72,9 @@ html_context = {
 #
 html_theme = 'sphinx_rtd_theme'
 
+# Favicon: organization avatar from GitHub (optional size for small icon)
+html_favicon = 'https://github.com/redup-ai.png?size=32'
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
