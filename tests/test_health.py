3f133b46f4d4db560038207fb5020f335a95f752
Andrii Hraboviy <35834412+andriygav@users.noreply.github.com>
2026-06-24 08:08:15 -0400

    try fix tests

--- tests/test_health.py
+++ tests/test_health.py
@@ -1,7 +1,11 @@
 import grpc
+import pytest
+
 from redup_servicekit.health import _configure_health_server
 
-def test_health_server():
+
+@pytest.mark.asyncio
+async def test_health_server():
     _configure_health_server(
         grpc.aio.server(), "TestServer"
     )
